5b7b908526cf8a210b5394130e2ab3ed82452f94
Khushi Pardeshi <pardeshikhushi23@gmail.com>
2024-07-02 01:12:09 +0530

    card added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,25 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
    <div>
-    <img src="/photo.jpg" alt="photo"></img>
+    <img src="/photo.jpg" className =" rounded-xl" alt="photo"></img>
+    <div className="grid grid-cols-12 pt-2">
+      <div className="col-span-1">
+      <img className="rounded-full w-12 h-12" src="/image.png" alt="photo"></img>
+      </div>
+      <div className="col-span-11 pl-2">
+        <div>
+        Hard mock coding interview
+        </div>
+        <div className="col-span-11  text-gray-400 text-base">
+         coding channel
+        </div>
+        <div className="col-span-11  text-gray-400 text-base">
+        46 Mn | 13 days ago
+        </div>
+      </div>
+      
+      
+    </div>
     
    </div>
   );
